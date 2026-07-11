a65d31d5d2eb1f1f1a5dec947649c607ca5c9dad
Gavin Peng <gpeng@oicr.on.ca>
2026-07-11 09:49:38 -0400

    use simmple calculate in regression test

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -6,6 +6,5 @@ set -o pipefail
 #enter the workflow's final output directory ($1)
 cd $1
 
-#find all files, return their md5sums to std out
-find . -name "*.vcf.gz" -xtype f -exec sh -c "zcat {} | grep -v ^# | md5sum" \;
+
 ls | sed 's/.*\.//' | sort | uniq -c
